0cf21c453bb0ce4e51214698045e2f009f1b34e8
Koji Saito <k-saito@cloudpack.jp>
2020-03-09 17:43:26 +0900

    Initialize repository

---
+++ dags_test_composer.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+
+import datetime
+
+from airflow import models
+from airflow.operators import bash_operator
+from airflow.operators import python_operator
+
+default_dag_args = {
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    'start_date': datetime.datetime(2018, 1, 1),
+}
+
+gcloud_command = """
+curl httpbin.org/ip
+gcloud config list
+gcloud beta compute --project "cloudpack-rd-vpcsc-private" ssh --zone "asia-northeast1-a" "clp-private-embulk01" --tunnel-through-iap -- "sudo su embulk -c 'id'"
+""".strip()
+
+# Define a DAG (directed acyclic graph) of tasks.
+# Any task you create within the context manager is automatically added to the
+# DAG object.
+with models.DAG(
+        'iret_composer_sample_gcloud_ssh',
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+    def greeting():
+        import logging
+        logging.info('Hello World!')
+
+    # An instance of an operator is called a task. In this case, the
+    # hello_python task calls the "greeting" Python function.
+    hello_python = python_operator.PythonOperator(
+        task_id='hello',
+        python_callable=greeting)
+
+    gcloud_ssh = bash_operator.BashOperator(
+        task_id='gcloud_ssh',
+        bash_command=gcloud_command
+    )
+
+    # Likewise, the goodbye_bash task calls a Bash script.
+    goodbye_bash = bash_operator.BashOperator(
+        task_id='bye',
+        bash_command='echo Goodbye.')
+
+    # Define the order in which the tasks complete by using the >> and <<
+    # operators. In this example, hello_python executes before goodbye_bash.
+    hello_python >> gcloud_ssh >> goodbye_bash
