c51475a9199763ec9dbab3f31d53f1da7c6aa51b
Koji Saito <k-saito@cloudpack.jp>
2020-03-17 16:11:38 +0900

    change start date

--- dags_test_composer.py
+++ dags_test_composer.py
@@ -14,7 +14,7 @@ default_dag_args = {
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2018, 1, 1, tzinfo=local_tz),
+    'start_date': datetime.datetime(2020, 3, 16, tzinfo=local_tz),
 }
 
 gcloud_command = """
